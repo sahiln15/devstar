c8792bf1852619de6cefa1a047bb86a9106ce0e2
Rafee104104 <tasnimmunawarrafee@gmail.com>
2024-07-06 13:44:56 +0600

    Lt

--- src/routes/(tools)/responsive-design-tester/+page.svelte
+++ src/routes/(tools)/responsive-design-tester/+page.svelte
@@ -17,15 +17,18 @@
     function dropDownPage(url){
       defaultHeight = 1920;
       defaultWidth = 1080;
-      outputUrl = "https://tailwindcss.com/docs/customizing-colors";
+      outputUrl = url;
     }
 
 </script>
 
+
+<!--
+
 <div class="container mx-auto max-w-screen-xl flex flex-col lg:flex-row overflow-hidden rounded-lg">
-    <!-- Add tool here -->
-    <!-- <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">  -->
-    <div class="side bg-blue-300 w-40 shadow-xl h-screen p-5 flex flex-col items-center sticky top-0">
+     Add tool here 
+     <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200"> 
+      <div class="side bg-blue-300 w-40 shadow-xl h-screen p-5 flex flex-col items-center sticky top-0">
         <div class="flex-1 flex flex-col items-center">
             <button>
                 <FontAwesomeIcon class="w-14 h-14" icon={faRotateLeft} />
@@ -45,12 +48,43 @@
         </div>
         
         <div class="content mt-10">
-          <!-- New UI elements will be added here -->
-          <iframe src={outputUrl} height={defaultHeight} width={defaultWidth} title="drop-down-page"></iframe>
-          
-      </div>
+           New UI elements will be added here 
+          <iframe src={outputUrl} height={defaultHeight} width={defaultWidth} title="drop-down-page"></iframe>          
+        </div>
     </main>
+
+
+-->
+
+<div class="container mx-auto max-w-screen-xl flex flex-col lg:flex-row overflow-hidden rounded-lg">
+  <!-- Add tool here -->
+  <div class="side bg-blue-300 w-40 shadow-xl h-screen p-5 flex flex-col items-center sticky top-0">
+      <div class="flex-1 flex flex-col items-center">
+          <button>
+              <FontAwesomeIcon class="w-14 h-14" icon={faRotateLeft} />
+          </button>
+          <button class="flex flex-col justify-between items-center py-24 text-lg">
+              <FontAwesomeIcon class="w-14 h-14" icon={faBars} />MENU
+          </button>
+      </div>
+      <button class="fa-bars">
+          <FontAwesomeIcon class="w-14 h-14" icon={faShareNodes} />
+      </button>
+  </div>
+  <main class="flex-1 p-5">
+      <div class="input-area mt-16 flex flex-row items-center">
+        <input bind:innerHTML={url} contenteditable type="text" placeholder="" class="input-url" />
+        <button on:click={()=>dropDownPage(url)} class="bg-black hover:bg-black-900 text-white w-11 h-11 rounded">Go</button>
+      </div>
+      <div class="content mt-10">
+          <!-- New UI elements will be added here -->
+          <iframe src={outputUrl} height={defaultHeight} width={defaultWidth} title="drop-down-page"></iframe>          
+      </div>
+  </main>
 </div>
+
+
+
 
 <style>
   .container{
@@ -83,3 +117,42 @@
       width: 100%;
   }
 </style>
+
+
+<!--
+
+
+<main class="container mx-auto max-w-screen-xl flex flex-col lg:flex-row overflow-hidden rounded-lg">
+ Add tool here 
+<div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">  
+    
+<div class="side bg-blue-300 w-40 shadow-xl h-screen p-5 flex flex-col items-center sticky top-0">
+  <div class="flex-1 flex flex-col items-center">
+    <button>
+      <FontAwesomeIcon class="w-14 h-14" icon={faRotateLeft} />
+    </button>
+    <button class="flex flex-col justify-between items-center py-24 text-lg">
+      <FontAwesomeIcon class="w-14 h-14" icon={faBars} />MENU
+    </button>
+  </div>
+  <div>
+    <button class="fa-bars">
+      <FontAwesomeIcon class="w-14 h-14" icon={faShareNodes} />
+    </button>
+  </div>
+  <div class="flex-1 p-5">
+    <div class="input-area mt-16 flex flex-row items-center">
+      <input bind:innerHTML={url} contenteditable type="text" placeholder="" class="input-url" />
+        <button on:click={()=>dropDownPage(url)} class="bg-black hover:bg-black-900 text-white w-11 h-11 rounded">Go</button>
+    </div>    
+    <div class="content mt-10">
+    New UI elements will be added here 
+      <iframe src={outputUrl} height={defaultHeight} width={defaultWidth} title="drop-down-page"></iframe>
+    </div>   
+  </div>
+</div>
+
+</main>
+
+
+-->
