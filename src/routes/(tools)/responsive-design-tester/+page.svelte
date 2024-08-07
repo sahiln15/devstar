9a1036770172f9a4c07617cea553947e8407735c
Rafee104104 <tasnimmunawarrafee@gmail.com>
2024-08-07 11:01:05 +0600

    LT Edit
    LT

--- src/routes/(tools)/responsive-design-tester/+page.svelte
+++ src/routes/(tools)/responsive-design-tester/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { faTabletScreenButton } from "@fortawesome/free-solid-svg-icons";
-	import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 	import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
 	import { faDesktop } from "@fortawesome/free-solid-svg-icons";
 	import { faMobile } from "@fortawesome/free-solid-svg-icons";
@@ -8,208 +7,101 @@
 	$: defaultWidth = null;
 	$: inputUrl = "";
 	$: storedUrl = "";
-	$: resDesktopIconClicked = false;
-	$: resTabletIconClicked = false;
-	$: resPhoneIconClicked = false;
-  
-  
-	let mobile = [
-	  { common: { width: 360, height: 640 } },
-	  { iphone6_to_8: { width: 375, height: 667 } },
-	  { larger: { width: 375, height: 720 } },
-	  { iphoneX: { width: 375, height: 812 } },
-	  { google_pixel_2: { width: 411, height: 731 } },
-	];
-	//i want to render a mini-sidebar such that when we click on the icons, it will show the various screens available
-  
-  
-	let resolutionObject = {
-	  twenty_two_inch_height: 1920,
-	  twenty_two_inch_width: 1200,
+	let clicked = {
+	  mobile: false,
+	  desktop: false,
+	  tablet: false,
 	};
-	function resolutionUpdate(height, width) {
-	  defaultHeight = height;
-	  defaultWidth = width;
+	let isButtonClicked = false;
+	let mobile = [
+	  { "Apple iPhone 3/4/4s": { width: 320, height: 480 } },
+	  { "Apple iPhone 5/5s": { width: 320, height: 568 } },
+	  { "Apple iPhone 6/6s/7": { width: 375, height: 667 } },
+	  { "Apple iPhone 6s Plus / 7 Plus": { width: 414, height: 736 } },
+	  { "Samsung Galaxy S5/S6/S7": { width: 360, height: 640 } },
+	  { "Sony Xperia Z2/Z3": { width: 360, height: 640 } },
+	  { "Google Pixel": { width: 411, height: 731 } },
+	  { "Nexus 4": { width: 384, height: 640 } },
+	  { "Samsung Galaxy S8": { width: 360, height: 740 } },
+	  { "OnePlus 8": { width: 412, height: 869 } },
+	];
+	let desktop = [
+	  { "24_desktop": { width: 1920, height: 1200 } },
+	  { "23_desktop": { width: 1920, height: 1080 } },
+	  { "22_desktop": { width: 1680, height: 1050 } },
+	  { "20_desktop": { width: 1600, height: 900 } },
+	  { "19_desktop": { width: 1440, height: 900 } },
+	  { "15_notebook": { width: 1366, height: 768 } },
+	  { "13_notebook": { width: 1024, height: 800 } },
+	  { "10_notebook": { width: 1024, height: 600 } },
+	  { "Apple Thunderbolt Display": { width: 2560, height: 1440 } },
+	  { "Dell UltraSharp Monitor": { width: 1440, height: 900 } },
+	];
+	let tablet = [
+	  { "Apple iPad Mini": { width: 768, height: 1024 } },
+	  { "Apple iPad Retina": { width: 768, height: 1024 } },
+	  { "Apple iPad Pro": { width: 1366, height: 1024 } },
+	  { "Amazon Kindle Fire": { width: 768, height: 1024 } },
+	  { "Amazon Kindle Fire HD": { width: 768, height: 1024 } },
+	  { "Asus Eee 1000": { width: 768, height: 1024 } },
+	  { "Nexus 7": { width: 600, height: 960 } },
+	  { "Nexus 9": { width: 1024, height: 768 } },
+	  { "Samsung Galaxy Tab S7": { width: 2560, height: 1600 } },
+	  { "Microsoft Surface Pro": { width: 2736, height: 1824 } },
+	];
+	function toggleSidebar(deviceType) {
+	  clicked[deviceType] = !clicked[deviceType];
+	  // Close other sidebars
+	  for (let key in clicked) {
+		if (key !== deviceType) {
+		  clicked[key] = false;
+		}
+	  }
 	}
 	function dropDownPage(inputUrl) {
-	  defaultHeight = 3840;
-	  defaultWidth = 2160;
-  
-  
-	  //3840
-	  //2160
-	  //1920
-	  //1080
-	  //https://learn.svelte.dev/tutorial/welcome-to-svelte
-  
-  
+	  defaultHeight = 1920;
+	  defaultWidth = 1080;
 	  storedUrl = inputUrl;
 	}
-	$: count = 0;
-  
-  
-	function incrementForDesktop() {
-	  if (count % 2 == 0) resDesktopIconClicked = true;
-	  else resDesktopIconClicked = false;
-	  count += 1;
-	}
-  
-  
-	function incrementForTablet() {
-	  count += 1;
-	  if (count % 2 == 0) incrementForDesktop = true;
-	  else incrementForDesktop = false;
-	}
-  
-  
-	function incrementForPhone() {
-	  count += 1;
-	  if (count % 2 == 0) resPhoneIconClicked = true;
-	  else resPhoneIconClicked = false;
+	function selectDevice(width, height) {
+	  defaultWidth = width;
+	  defaultHeight = height;
+	  isButtonClicked = !isButtonClicked;
+	  for (let key in clicked) {
+		clicked[key] = false;
+	  }
 	}
   </script>
-  
-  
   <div
 	class="container mx-auto max-w-screen-xl flex flex-col lg:flex-row overflow-hidden rounded-lg"
   >
-	{#if resDesktopIconClicked == true}
-	  <div
-		class=" side hover:bg-emerald-400 bg-emerald-300 w-40 shadow-xl h-screen p-5 flex flex-col items-center py-48"
-	  >
-		<button
-		  on:click={() => resolutionUpdate(1920, 1200)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  24" Desktop<br />
-			  <p style="color: blue;" class="resolution">1920x1200</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1920,1080)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  23" Desktop<br />
-			  <p style="color: blue;" class="resolution">1920x1080</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1680,1050)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  22" Desktop<br />
-			  <p style="color: blue;" class="resolution">1680x1050</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1920, 1200)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  21.5" Desktop<br />
-			  <p style="color: blue;" class="resolution">1080x1920</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1600,900)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  20" Desktop<br />
-			  <p style="color: blue;" class="resolution">1600x900</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1440,900)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  19" Desktop<br />
-			  <p style="color: blue;" class="resolution">1440x900</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1366,768)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  15" Desktop<br />
-			  <p style="color: blue;" class="resolution">1366x768</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1024,800)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  13" Desktop<br />
-			  <p style="color: blue;" class="resolution">1024x800</p>
-			</li>
-		  </ul>
-		</button>
-		<button
-		  on:click={() => resolutionUpdate(1024,600)}
-		  class="w-40 hover:bg-emerald-500"
-		>
-		  <ul class=" submenu-desktop">
-			<li style="color: black" class="submenu-title">
-			  10" Desktop<br />
-			  <p style="color: blue;" class="resolution">1024x600</p>
-			</li>
-		  </ul>
-		</button>
-	   
-	  </div>
-	{/if}
-  
-  
-   
-  
-  
-	<!-- Add tool here -->
 	<div
-	  class="side bg-blue-300 w-40 shadow-xl h-screen p-5 flex flex-col items-center py-48"
+	  class="side bg-gray-400 w-40 shadow-xl min-h-screen top-0 left-0 bottom-0"
 	>
-	  <!--<button>
-		<FontAwesomeIcon class="w-14 h-14" icon={faRotateLeft} style="top"/>
-	  </button>-->
-  
-  
-	 
-  
-  
-	  <button on:click={incrementForDesktop}>
-		<FontAwesomeIcon class="w-14 h-14" icon={faDesktop} />
+	  <button
+		class="flex flex-col justify-between items-center"
+		on:click={() => toggleSidebar("mobile")}
+	  >
+		<FontAwesomeIcon class="w-14 h-14 py-6 px-14" icon={faMobile} />
 	  </button>
-	  <button class="flex flex-col justify-between items-center py-4">
-		<FontAwesomeIcon class="w-14 h-14" icon={faMobile} />
+	  <button
+		class="flex flex-col justify-between items-center"
+		on:click={() => toggleSidebar("desktop")}
+	  >
+		<FontAwesomeIcon class="w-14 h-14 py-6 px-14" icon={faDesktop} />
 	  </button>
-  
-  
-	  <button class="fa-bars">
-		<FontAwesomeIcon class="w-14 h-14" icon={faTabletScreenButton} />
+	  <button
+		class="flex flex-col justify-between items-center"
+		on:click={() => toggleSidebar("tablet")}
+	  >
+		<FontAwesomeIcon
+		  class="w-14 h-14 py-6 px-14"
+		  icon={faTabletScreenButton}
+		/>
 	  </button>
 	</div>
-	<main class="flex-1 p-5">
-	  <div class="input-area mt-16 flex flex-row items-center">
+	<main class="flex-1 py-16 w-screen">
+	  <div class="input-area mt-16 sticky flex flex-row items-center">
 		<input
 		  type="text"
 		  bind:value={inputUrl}
@@ -232,54 +124,142 @@
 		>
 		</object>
 	  </div>
+	  <div class="mini-sidebar">
+		{#if clicked.mobile}
+		  <div class="submenu">
+			<ul>
+			  {#each mobile as device}
+				{#each Object.entries(device) as [key, value]}
+				  <button
+					class="block w-full mb-4 text-left font-bold"
+					on:click={() =>
+					  selectDevice(value.width, value.height)}
+				  >
+					{key}<br />
+					<p class="text-sm text-gray-500">
+					  {value.width}x{value.height}
+					</p>
+				  </button>
+				{/each}
+			  {/each}
+			</ul>
+		  </div>
+		{/if}
+		{#if clicked.desktop}
+		  <div class="submenu">
+			<ul>
+			  {#each desktop as device}
+				{#each Object.entries(device) as [key, value]}
+				  <button
+					class="block w-full mb-4 text-left font-bold"
+					on:click={() =>
+					  selectDevice(value.width, value.height)}
+				  >
+					{key}<br />
+					<p class="text-sm text-gray-500">
+					  {value.width}x{value.height}
+					</p>
+				  </button>
+				{/each}
+			  {/each}
+			</ul>
+		  </div>
+		{/if}
+		{#if clicked.tablet}
+		  <div class="submenu">
+			<ul>
+			  {#each tablet as device}
+				{#each Object.entries(device) as [key, value]}
+				  <button
+					class="block w-full h-32 text-left font-bold"
+					on:click={() =>
+					  selectDevice(value.width, value.height)}
+				  >
+					{key}<br />
+					<p class="text-sm text-gray-500">
+					  {value.width}x{value.height}
+					</p>
+				  </button>
+				{/each}
+			  {/each}
+			</ul>
+		  </div>
+		{/if}
+	  </div>
 	</main>
   </div>
   
-  
   <style>
+	* {
+	  box-sizing: border-box;
+	  padding: 0;
+	  margin: 0;
+	}
 	.container {
-	  background-color: white;
-	  max-height: 100%;
-	  max-width: 100%;
-	  border: 5px ridge blue;
+	  position: relative;
+	  background-color: rgb(161, 157, 157);
+	  max-height: 100vh;
+	  justify-content: center;
 	}
 	.side {
+	  position: relative;
+	  padding-top: 15rem;
 	  box-shadow: 0 -1px 15px 2px rgb(0 0 0 / 0.7);
 	}
-	.fa-bars {
-	  color: rgb(0, 0, 0);
+	.input-area {
+	  position: fixed;
+	  position: absolute;
+	  transform: translate(1rem, -26rem);
 	}
 	.input-url {
-	  background-color: rgb(183, 231, 233);
+	  background-color: rgb(202, 222, 223);
 	  color: rgb(71, 69, 69);
 	  padding: 10px;
 	  padding-right: 0;
 	  border: none;
 	  border-radius: 5px;
-	  width: 100%;
+	  width: 52rem;
 	}
-	@media (min-width: 1024px) {
-	  .container {
-		flex-direction: row;
-	  }
+	.submenu ul {
+	  position: absolute;
+	  top: 0;
+	  right: 0;
+	  color: rgb(255, 255, 255);
+	  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	  z-index: 10;
+	  width: 170px;
+	  height: 55rem;
+	  padding-top: 0.4rem;
+	  border-left: 1px solid #ccc;
+	  transform: translate(-59.47rem, 0.01rem);
+	  background-color: rgb(34, 34, 34);
+	  overflow-y: auto;
+	  scrollbar-width: none;
 	}
-	.input-area {
+	.submenu ul button {
+	  padding-left: 0.6rem;
+	  margin-bottom: 3rem;
+	  border-bottom: 1px solid rgb(255, 255, 255);
+	  font-weight: 300;
+	}
+	main {
 	  display: flex;
-	  gap: 10px; /* Adjust gap as needed */
 	  align-items: center;
-	  width: 100%;
+	  justify-content: center;
+	  margin-top: 8rem;
+	  margin-left: 2rem;
+	  overflow: auto;
+	  overflow-x: auto;
 	}
-  
-  
 	.content {
-		scroll-behavior: smooth;        
-        scrollbar-width: 2rem;
-        overflow-x: auto;
-        overflow-y: auto;
-        max-height:max-content;
-        max-width: max-content;
+	  scroll-behavior: smooth;
+	  scrollbar-width: 2rem;
+	  overflow-x: auto;
+	  overflow-y: auto;
+	  max-height: max-content;
+	  max-width: max-content;
 	}
-  
+ 
   
 	.submenu-desktop {
 	  align-items: center;
